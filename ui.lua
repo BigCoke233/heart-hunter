65a24f2f8f8c6499df86bcb5cd909fccb7824bc1
Eltrac <hi@guhub.cn>
2025-02-14 17:10:34 +0800

    feat(ui): show current ammo ammount

---
+++ ui.lua
@@ -0,0 +1,14 @@
+local ui = {}
+
+function ui.drawAmmoBar()
+    local height = 30
+    local y = love.graphics.getHeight() - height
+    local x = 10
+    local gap = 20
+
+    for i, v in pairs(ammo) do
+        love.graphics.draw(bulletImage[v], x+gap*(i-1), y)
+    end
+end
+
+return ui
