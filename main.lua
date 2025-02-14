e7e2e4ae6e20339b319dd200b90b30f2cd1b2aac
Eltrac <hi@guhub.cn>
2025-02-14 17:00:24 +0800

    feat: add ammo limits

--- main.lua
+++ main.lua
@@ -3,7 +3,6 @@ bullets = require "bullets"
 playerObject = require "player"
 
 function love.load()
-    player.sprite = love.graphics.newImage("resources/sprites/oldman.png")
     bulletImage = {
         redheart = love.graphics.newImage("resources/sprites/heart.png")
     }
--- moves.lua
+++ moves.lua
@@ -1,18 +1,30 @@
 local moves = {}
 
+ammo = { "redheart", "redheart", "redheart" } -- init ammo stack
+bulletSpeed = 300
+
 function moves.fireSlingshot(x, y, playerX, playerY, shots)
-    local vx = x - playerX
-    local vy = y - playerY
-    local length = math.sqrt(vx*vx + vy*vy)
+    if (#ammo == 0) then
+        print("out of ammo!")
+        return
+    end
 
-    if length==0 then return end
-    local sin = vx / length
-    local cos = vy / length
+    -- essential calculation
+    local function calculateShooting()
+        local vx = x - playerX
+        local vy = y - playerY
+        local length = math.sqrt(vx*vx + vy*vy)
 
-    bulletSpeed = 300
+        if length==0 then return end
 
+        return vx / length, vy / length
+    end
+    local sin, cos = calculateShooting()
+
+    -- fire the bullet
+    local currentBullet = table.remove(ammo)
     table.insert(shots, {
-        type = "redheart",
+        type = currentBullet,
         speed = { x = sin*bulletSpeed, y = cos*bulletSpeed },
         orientation = math.asin(sin),
         currentPos = { x = playerX, y = playerY },
