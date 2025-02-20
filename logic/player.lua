9f76f5ef1776321378ac522672a9835e7c6b4a54
Eltrac <hi@guhub.cn>
2025-02-20 17:20:30 +0800

    chore: mark buggy code

--- logic/player.lua
+++ logic/player.lua
@@ -24,11 +24,12 @@ local function playerMoves(dt)
         if love.keyboard.isDown(move.key) then
             if utils.hitObstacleAt(move.dir, G.player[move.axis], G.player.body.r) then return end
 
-            for _, obstacle in ipairs(G.currentRoom.obstacles) do
-                if obstacle.body:collide(G.player.body, obstacle.x, obstacle.y, G.player.x, G.player.y) then
-                    G.player[move.axis] = G.player[move.axis] - G.player.speed * dt * move.delta
-                end
-            end
+            -- TODO NEED FIX --
+            -- for _, obstacle in ipairs(G.currentRoom.obstacles) do
+            --     if obstacle.body:collide(G.player.body, obstacle.x, obstacle.y, G.player.x, G.player.y) then
+            --         G.player[move.axis] = G.player[move.axis] - G.player.speed * dt * move.delta
+            --     end
+            -- end
 
             G.player[move.axis] = G.player[move.axis] + G.player.speed * dt * move.delta
         end
