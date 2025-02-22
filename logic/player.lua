e8ba7652c74c683fb758239ee9d79cf07bc7520a
Eltrac <hi@guhub.cn>
2025-02-22 10:44:32 +0800

    refactor: isBlocked() function in player logic

--- logic/player.lua
+++ logic/player.lua
@@ -12,31 +12,35 @@ end
 
 -- state update functions
 
-local function meetWall(d, axis, dv)
-    local borders = G.currentRoom.borders
-    local r, axisVal = G.player.body.r, G.player[axis]+dv
+local function isBlocked(dir, axis, dv)
+    local function meetWall(d, axis, dv)
+        local borders = G.currentRoom.borders
+        local r, axisVal = G.player.body.r, G.player[axis]+dv
 
-    local hitRoomBorder = false
-    if d==Direction.RIGHT then
-        hitRoomBorder = axisVal+r >= borders[Direction.RIGHT]
-    elseif d==Direction.LEFT then
-        hitRoomBorder = axisVal-r <= borders[Direction.LEFT]
-    elseif d==Direction.TOP then
-        hitRoomBorder = axisVal-r <= borders[Direction.TOP]
-    elseif d==Direction.BOTTOM then
-        hitRoomBorder = axisVal+r >= borders[Direction.BOTTOM]
-    end
-
-    return hitRoomBorder
-end
-
-local function meetObstacle(dv, dir)
-    for _, obstacle in ipairs(G.currentRoom.obstacles) do
-        if obstacle:isMet(G.player.body, G.player.x, G.player.y, dv, dir) then
-            return true
+        local hitRoomBorder = false
+        if d==Direction.RIGHT then
+            hitRoomBorder = axisVal+r >= borders[Direction.RIGHT]
+        elseif d==Direction.LEFT then
+            hitRoomBorder = axisVal-r <= borders[Direction.LEFT]
+        elseif d==Direction.TOP then
+            hitRoomBorder = axisVal-r <= borders[Direction.TOP]
+        elseif d==Direction.BOTTOM then
+            hitRoomBorder = axisVal+r >= borders[Direction.BOTTOM]
         end
+
+        return hitRoomBorder
     end
-    return false
+
+    local function meetObstacle(dv, dir)
+        for _, obstacle in ipairs(G.currentRoom.obstacles) do
+            if obstacle:isMet(G.player.body, G.player.x, G.player.y, dv, dir) then
+                return true
+            end
+        end
+        return false
+    end
+
+    return meetWall(dir, axis, dv) or meetObstacle(dv, dir)
 end
 
 local function playerMoves(dt)
@@ -50,7 +54,7 @@ local function playerMoves(dt)
     for _, move in ipairs(moveDirections) do
         local dv = G.player.speed * dt * move.delta
         if love.keyboard.isDown(move.key) then
-            if meetWall(move.dir, move.axis, dv) or meetObstacle(dv, move.dir) then return end
+            if isBlocked(move.dir, move.axis, dv) then return end
             G.player[move.axis] = G.player[move.axis] + G.player.speed * dt * move.delta
         end
     end
