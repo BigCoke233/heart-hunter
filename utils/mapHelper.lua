d7b96e9d44599705da14058f23586c6f5be66d1d
Eltrac <hi@guhub.cn>
2025-03-09 13:12:56 +0800

    chore: remove testing prints

--- utils/mapHelper.lua
+++ utils/mapHelper.lua
@@ -5,16 +5,6 @@ function mapHelper.getAvailablePairedDirections(room1, room2)
     local dd2 = room2:getDoorlessDirections()
     local pairedDirections = {}
 
-    print("room 1 doors")
-    for _, door in ipairs(room1.doors) do
-        print(door.location)
-    end
-
-    print("room 2 doors")
-    for _, door in ipairs(room2.doors) do
-        print(door.location)
-    end
-
     for _, dir1 in ipairs(dd1) do
         local dir2 = OppositeDirection[dir1]
         if utils.contains(dd2, dir2) then
