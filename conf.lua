24abb521363788162da14ba7716fce6c9ba551a8
Eltrac <hi@guhub.cn>
2025-03-10 21:12:11 +0800

    chore: prepare game distribution

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.identity = "heart-hunter"
+    t.version = "11.5"
+    t.window.title = "Heart Hunter"
+    t.modules.joystick = false
+    t.modules.physics = false
+end
