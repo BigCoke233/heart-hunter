4bfe8d7679bfce861c03cff728d1aa9f44efeff9
Eltrac <hi@guhub.cn>
2025-03-12 17:53:56 +0800

    refactor: remove unused method

--- objects/Obstacle.lua
+++ objects/Obstacle.lua
@@ -30,21 +30,6 @@ function Obstacle:getBorders()
     return { left, right, top, bottom }
 end
 
-function Obstacle:isMet(body, currentX, currentY, dv, dir)
-    -- x,y should be the predicted next location of this body
-    -- dv = speed * dt * direction
-    local x, y
-    if dir == Direction.LEFT or dir == Direction.RIGHT then
-        x = currentX + dv
-        y = currentY
-    elseif dir == Direction.UP or dir == Direction.DOWN then
-        x = currentX
-        y = currentY + dv
-    end
-
-    return self.body:collide(body, self.x, self.y, x, y)
-end
-
 function Obstacle:update(dt)
     -- nothing yet
 end
